5012a493c64d64ca7a92e38f27327a9bd2429e7c
Allan Fernandes <allan.fernandes@responsiblelife.co.uk>
2024-12-02 08:06:05 +0000

    refactor: update global styles and improve layout structure; add Geist font integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
 import {FloatingMenu} from "@/components/floatingMenu";
+import wavingHand from "@/app/assets/waving-hand.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-black">
+    <div className="max-w-2xl mt-16 mx-auto px-5 bg-white dark:bg-black space-y-4">
+      <h2 className="text-black dark:text-white font-bold tracking-tighter sm:text-5xl text-3xl flex items-center">Hi, I'm Allan
+        <span className="flex"><Image src={wavingHand} alt={wavingHand} width={40} height={40} title="Waving hand" className="ml-4 inline"/></span>
+      </h2>
+      <p className="text-black dark:text-white md:text-xl">
+        a mediocre front-end developer, I like b̶r̶e̶a̶k̶i̶n̶g̶&nbsp; building cool things that live on the internet.
+      </p>
+      <h3 className="text-black dark:text-white font-bold text-xl">About</h3>
+      <p className="text-pretty text-black dark:text-white md:text-m">
+        I've been building websites for nearly four years often using vanilla javascript, React, (S)CSS and other web technologies.
+        With a master’s degree in Computer Science, I am now a web developer at Responsible Life, where I work as part of a dynamic team focused on crafting innovative web applications to transform the equity release space.
+      </p>
+      <p>I also play football, listen to afrobeats and go on runs to to get my ass of the computer.</p>
       <FloatingMenu />
     </div>
   );

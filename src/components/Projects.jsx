1bd41804fce766525bfa05e53629e589fbe38960
Allan Fernandes <Fernzallan14@gmail.com>
2024-12-13 22:08:40 +0000

    feat: add new projects to showcase; enhance project details and include website links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { projects } from '@/app/utils';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Projects = () => {
     return (
@@ -14,39 +15,69 @@ const Projects = () => {
             <p className="text-pretty text-black dark:text-white md:text-lg my-4 text-center">
                 I've worked on a variety of projects, from simple websites to complex web applications. Here are few of my finest.
             </p>
-            <div className='grid grid-cols-2 gap-6'>
+            <div className='grid grid-cols-2 gap-6 mb-6'>
                 {
                     projects.map((project, index) => {
                         return (
                             <div
                                 key={index}
-                                className="flex flex-col gap-4 rounded-xl bg-card dark:bg-card-dark dark:bg-white dark:text-black"
+                                className="items-center gap-4 rounded-xl bg-card dark:bg-card-dark dark:bg-white dark:text-black shadow-lg h-[100%] flex flex-col"
                             >
                                 <Image
                                     src={project.image}
-                                    alt={project.company}
+                                    alt={project.title}
                                     width={500}
                                     height={50}
-                                    className="object-cover h-[200px] rounded-lg"
+                                    className="object-cover h-[200px]"
                                 />
-                                <div className="p-4">
-                                    <h3 class="font-semibold tracking-tight mt-1 text-base">
+                                <div className="p-4 flex flex-col h-[100%]">
+                                    <h3 className="font-semibold tracking-tight mt-1 text-base">
                                         {project.title}
                                     </h3>
                                     <p className="text-sm">{project.date}</p>
-                                    <p className='text-sm my-2'>{project.summary}</p>
-                                    <div className="flex gap-2 my-4">
+                                    <p className="text-sm my-2">{project.summary}</p>
+                                    <div className="flex gap-2 my-4 flex-wrap ]">
                                         {project.tools.map((tool, index) => {
                                             return (
                                                 <p
                                                     key={index}
-                                                    className="text-xs dark:bg-black dark:text-white bg-white text-black font-medium text-md rounded-lg p-2 h-fit"
+                                                    className="text-xs dark:text-white bg-black text-white font-medium text-md rounded-lg p-2 h-fit shadow dark:bg-gray-800 dark:border-gray-700"
                                                 >
                                                     {tool}
                                                 </p>
                                             );
                                         })}{' '}
                                     </div>
+                                    <Link
+                                        href={project.website}
+                                        target="_blank"
+                                        className="mt-auto text-white dark:text-white hover:underline gap-2 text-sm"
+                                    >
+                                        <p
+                                            key={index}
+                                            className="text-xs dark:text-white bg-black text-white font-medium text-md w-fit rounded-lg p-2 h-fit shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 d-flex"
+                                        >
+                                            <span className='flex gap-2 items-center'>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="24"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    stroke-width="2"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    class="lucide lucide-globe size-3"
+                                                >
+                                                    <circle cx="12" cy="12" r="10"></circle>
+                                                    <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path>
+                                                    <path d="M2 12h20"></path>
+                                                </svg>
+                                                Website
+                                            </span>
+                                        </p>
+                                    </Link>
                                 </div>
                             </div>
                         );

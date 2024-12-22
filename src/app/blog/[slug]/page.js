b685d374aa7533cd0ce05c455a575134e1c10fae
Allan Fernandes <Fernzallan14@gmail.com>
2024-12-22 23:03:14 +0000

    minor-changes

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,12 +1,13 @@
 
 import { getPostData } from "@/app/lib/posts";
+import { formatDate } from "@/app/utils";
 import Link from 'next/link';
 
 export default async function PostPage({ params }) {
     const postData = await getPostData(params.slug);
     return (
         <div>
-            <Link href="/blog">
+            <Link href="/blog" className="absolute top-0 left-0 mt-4 ml-4">
                 {' '}
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -25,8 +26,12 @@ export default async function PostPage({ params }) {
             </Link>
 
             <article>
-                <h1>{postData.title}</h1>
-                <p>{postData.date}</p>
+                <h2 className=" font-bold text-pantone sm:text-3xl text-3xl flex items-center">
+                    {postData.title}
+                </h2>
+                <p>
+                    {formatDate(postData.date)}
+                </p>
                 <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
             </article>
         </div>
--- src/app/blog/layout.js
+++ src/app/blog/layout.js
@@ -2,9 +2,11 @@ import { FloatingMenu } from "@/components/floatingMenu";
 
 export default function RootLayout({ children }) {
     return (
-        <div className="max-w-2xl mt-16 mx-auto px-5 bg-white dark:bg-black space-y-4 font-sans text-black dark:text-white">
-            {children}
-            <FloatingMenu />
-        </div>
+        <>
+            <div className="max-w-2xl mt-16 mx-auto px-5 bg-white dark:bg-black space-y-4 font-sans text-black dark:text-white">
+                {children}
+                <FloatingMenu />
+            </div>
+        </>
     );
 }

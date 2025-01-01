2bdf473f8147268f1a28ecfe28f7549ea56e14dd
allanmosesfernandes <fernzallan14@gmail.com>
2025-01-01 09:41:55 +0530

    feat: enhance blog post layout; improve styling and readability of post summaries and tags

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -3,7 +3,6 @@ import { getSortedPostsData } from '../lib/posts';
 import { formatDate } from '../utils';
 
 const Blog = () => {
-
     const allPostsData = getSortedPostsData();
 
     return (
@@ -17,47 +16,56 @@ const Blog = () => {
             </p>
             <ul className="mt-8">
                 {allPostsData.map(({ id, title, date, tags, slug, summary }) => (
-                    <Link href={`/blog/${slug}`} key={id} className="bg-slate-">
-                        <li key={id} className="blog-li ">
+                    <div key={id}>
+                        <li
+                            key={id}
+                            className="blog-li shadow-md p-4 shadow-pantone hover:shadow-indigo-500/40"
+                        >
                             <div className="flex justify-between">
                                 <h3 className="font-bold">{title}</h3>
                                 <p className="text-pantone">{formatDate(date)}</p>
                             </div>
-                            <p className="my-4">{summary}</p>
-                            <div className="flex gap-2 items-center mb-4">
-                                <h4 className="text-pantone font-bold text-md">Read more</h4>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="#FF5A26"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="lucide lucide-chevron-right size-4 transform transition-all duration-300 ease-out"
-                                >
-                                    <path d="m9 18 6-6-6-6"></path>
-                                </svg>
-                            </div>
-                            {tags &&
-                                tags.map((tag, index) => (
-                                    <span
-                                        key={index}
-                                        className="dark:bg-white dark:text-black bg-black text-white font-medium text-md rounded-lg px-2 py-1 mr-2"
+                            <p className="my-6">{summary}</p>
+                            <div className="flex gap-2 items-center justify-between mt-4">
+                                <div className="flex gap-2 items-center">
+                                    <Link href={`/blog/${slug}`}>
+                                        <h4 className="text-pantone font-bold text-md">
+                                            Read more
+                                        </h4>
+                                    </Link>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="#FF5A26"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="lucide lucide-chevron-right size-4 transform transition-all duration-300 ease-out"
                                     >
-                                        {tag}
-                                    </span>
-                                ))}
+                                        <path d="m9 18 6-6-6-6"></path>
+                                    </svg>
+                                </div>
+                                <div>
+                                    {tags &&
+                                        tags.map((tag, index) => (
+                                            <span
+                                                key={index}
+                                                className="dark:bg-white dark:text-black bg-black text-white font-medium text-md rounded-lg px-2 py-1 mr-2"
+                                            >
+                                                {tag}
+                                            </span>
+                                        ))}
+                                </div>
+                            </div>
                         </li>
-                    </Link>
+                    </div>
                 ))}
             </ul>
         </div>
     );
-}
+};
 
-export default Blog
-
-
+export default Blog;

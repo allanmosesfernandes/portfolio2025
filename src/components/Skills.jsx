9c30cc755ecae10dc842a834c0732ab37895a0a8
allanmosesfernandes <fernzallan14@gmail.com>
2025-01-04 01:50:12 +0530

    Vercel - Develop. Preview. Ship.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,9 +7,9 @@ const Skills = () => {
             <h3 className="text-black dark:text-white font-bold text-2xl mt-6">Skills</h3>
             <div className="flex gap-4 w-full flex-wrap">
                 <p className="text-pretty text-black dark:text-white md:text-lg my-4">
-                    Programming languages, frameworks, tools and technologies I've had hands-down
-                    experience with and while I won't claim mastery in all, I've fearlessly delved
-                    into their realm, expanding my skills.
+                    Programming languages, frameworks, tools and technologies I&apos;ve had hands-down
+                    experience with and while I won&apos;t claim mastery in all, I&apos;ve
+                    fearlessly delved into their realm, expanding my skills.
                 </p>
                 <div className="flex gap-2 flex-wrap justify-center">
                     {skills.map((skill, index) => {

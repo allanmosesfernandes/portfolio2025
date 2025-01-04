8aef522f17cd9fd7ad6c8cfa61cd3dac34ebf5aa
allanmosesfernandes <fernzallan14@gmail.com>
2025-01-04 17:18:14 +0530

    Favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
       <html lang="en" className="dark">
+          <head>
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🦡</text></svg>"></link>
+          </head>
           <body className={`bg-white dark:bg-black antialiased`}>
               <div className="max-w-2xl sm:mt-16 mt-4 mx-auto px-5 bg-white dark:bg-black font-sans">
                   {children}

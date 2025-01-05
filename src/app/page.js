bd126c28d3c1fcb5bb56293b0b397c48570d253f
allanmosesfernandes <fernzallan14@gmail.com>
2025-01-05 18:48:07 +0530

    OG image issue

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import Homepage from "@/components/Homepage";
 import { getSortedPostsData } from "./lib/posts";
 
 export const metadata = {
-  title: "Home - Allan Fernandes",
-  description: "Allan Fernandes is a frontend developer based in Nottingham, UK. He is a Computer Science post graduate from NTU. He is currently working as a web developer at Responsible Life.",
-  image: "/og-image.png",
+    title: "Home - Allan Fernandes",
+    description: "Allan Fernandes is a frontend developer based in Nottingham, UK. He is a Computer Science post graduate from NTU. He is currently working as a web developer at Responsible Life.",
+    image: "/og-image.png",
 };
 
 export default function Home() {

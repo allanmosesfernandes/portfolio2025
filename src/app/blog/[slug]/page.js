e9b47a8cdf067cdb2d38897d6b7dbe6283805358
allanmosesfernandes <fernzallan14@gmail.com>
2025-01-22 02:51:44 +0530

    Arrow color

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -55,7 +55,7 @@ export default async function PostPage({ params }) {
                             fillRule="evenodd"
                             clipRule="evenodd"
                             d="M6.3508 12.7499L11.2096 17.4615L10.1654 18.5383L3.42264 11.9999L10.1654 5.46148L11.2096 6.53833L6.3508 11.2499L21 11.2499L21 12.7499L6.3508 12.7499Z"
-                            fill="#FF5A26"
+                            fill="#646cff"
                         />
                     </svg>
                 </Link>
